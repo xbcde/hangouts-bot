3dee1cc0bc1187fef752c2e93267cd5623e0fd4d
Victor Liu <victorjqliu@gmail.com>
2023-04-18 15:04:31 -0700

    feat: check in server skeleton

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import os
+
+import uvicorn as uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=int(os.environ.get("UVICORN_PORT", "8000")),
+    )
