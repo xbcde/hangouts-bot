0fcd303a3fff36d8e1adb20d8deda374d130ead6
Victor Liu <victorjqliu@gmail.com>
2023-04-18 23:37:46 -0700

    deploy: change main server port from 8000 to 80

--- src/main.py
+++ src/main.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=int(os.environ.get("UVICORN_PORT", "8000")),
+        port=int(os.environ.get("UVICORN_PORT", "80")),
     )
